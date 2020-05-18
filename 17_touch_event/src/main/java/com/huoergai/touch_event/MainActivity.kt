de72ebc0a93de8ecff2c68f20479f605bfd8114a
huoergai <huoergai@gmail.com>
2020-05-18 11:18:34 +0800

    1.17 触摸事件在 View 和 ViewGroup 中的传递过程 2.触摸事件的消费过程

--- 17_touch_event/src/main/java/com/huoergai/touch_event/MainActivity.kt
+++ 17_touch_event/src/main/java/com/huoergai/touch_event/MainActivity.kt
@@ -10,6 +10,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+         * 1.触摸事件的消费顺序是：从上层子 View 往下到根 View
+         * 2.一旦在中间被消费(在 OnTouchEvent 的 ACTION_DOWN 事件中返回了 true), 则不再往下传递,
+         *   该同一组或同一序列的后续事件也不再向下传递。
+         * 3.getAction() 与 getActionMasked(): 后者有对多点触控的支持,除了能获取到事件类型，还能获取手指 index 等信息
+         * 4.ViewGroup onInterceptTouchEvent() 负责是否拦截子 View 的事件, 拦截后则其 onTouchEvent() 负责处理该事件
+         * 5.如果 ViewGroup 拦截了事件, 需要在拦截时记录下按下的数据,以供后面消费事件使用
+         *
+         * 触摸事件传递流程简化
+         * View.dispatchTouchEvent():
+         * public boolean dispatchTouchEvent(){
+         *      return onTouchEvent();
+         * }
+         *
+         * ViewGroup.dispatchTouchEvent()
+         * public boolean ViewGroup.dispatchTouchEvent(){
+         *      boolean result;
+         *      if(interceptTouchEvent()){
+         *          result = onTouchEvent();
+         *      }else{
+         *          result = 子 View 的 dispatchTouchEvent()
+         *      }
+         *      return result
+         * }
+         *
+         */
+
+
+
     }
 
     fun onClick(view: View) {
