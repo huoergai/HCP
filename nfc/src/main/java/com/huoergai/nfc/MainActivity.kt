6631a07cf4c09b2b99d007e2742c407e92573054
huoergai <huoergai@gmail.com>
2020-07-16 03:31:47 +0800

    1.nfc module init

---
+++ nfc/src/main/java/com/huoergai/nfc/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.huoergai.nfc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * android nfc 开发相关
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+}
---
+++ nfc/src/main/java/com/huoergai/nfc/NfcActivity.kt
@@ -0,0 +1,17 @@
+package com.huoergai.nfc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * D&T: 2020-07-13 21:15
+ * Des:
+ */
+class NfcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nfc)
+
+
+    }
+}
